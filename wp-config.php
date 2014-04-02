d179bb4bc825fe73a5f9a97f3b722b82ac69b450
Josh Koenig <josh@getpantheon.com>
2014-04-02 10:21:49 -0700

    Remove silly extra whitespace.

--- wp-config.php
+++ wp-config.php
@@ -105,7 +105,7 @@ endif;
  * You can have multiple installations in one database if you give each a unique
  * prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'wp_';
+$table_prefix = 'wp_';
 
 /**
  * WordPress Localized Language, defaults to English.
