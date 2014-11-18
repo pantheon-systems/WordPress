3bc22fc52213b8cbf6238fe299112da18f23c351
Matthew Boynes <mboynes@alleyinteractive.com>
2014-11-18 12:58:44 -0500

    Fix bug with how Pantheon is disabling updates

--- wp-content/mu-plugins/pantheon/pantheon-updates.php
+++ wp-content/mu-plugins/pantheon/pantheon-updates.php
@@ -4,7 +4,16 @@ if ( in_array( $_ENV['PANTHEON_ENVIRONMENT'], Array('test', 'live') ) ) {
   //
   // Disable Core Updates EVERYWHERE (use git upstream)
   //
-  add_filter( 'pre_site_transient_update_core', create_function( '$a', "return null;" ) );
+  function _pantheon_disable_wp_updates() {
+    include ABSPATH . WPINC . '/version.php';
+    return (object) array(
+	    'updates' => array(),
+	    'version_checked' => $wp_version,
+	    'last_checked' => time(),
+    );
+  }
+
+  add_filter( 'pre_site_transient_update_core', '_pantheon_disable_wp_updates' );
 
   //
   // Disable Plugin Updates
@@ -15,12 +24,12 @@ if ( in_array( $_ENV['PANTHEON_ENVIRONMENT'], Array('test', 'live') ) ) {
   }
 
   remove_action( 'load-update-core.php', 'wp_update_plugins' );
-  add_filter( 'pre_site_transient_update_plugins', create_function( '$a', "return null;" ) );
+  add_filter( 'pre_site_transient_update_plugins', '_pantheon_disable_wp_updates' );
 
   //
   // Disable Theme Updates
   //
   remove_action( 'load-update-core.php', 'wp_update_themes' );
-  add_filter( 'pre_site_transient_update_themes', create_function( '$a', "return null;" ) );
+  add_filter( 'pre_site_transient_update_themes', '_pantheon_disable_wp_updates' );
 }
 
