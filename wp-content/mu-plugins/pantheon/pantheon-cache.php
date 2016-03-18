28bff93159139181eb6b2355ba853ce83f949e35
fusionx1 <paul.depaula@gmail.com>
2016-03-19 03:57:56 +0800

    Changed from delete_others_posts capability to manage options

--- wp-content/mu-plugins/pantheon/pantheon-cache.php
+++ wp-content/mu-plugins/pantheon/pantheon-cache.php
@@ -228,7 +228,7 @@ class Pantheon_Cache {
  		if ( ! is_user_logged_in() )
  			return false;
 
- 		if ( function_exists( 'current_user_can' ) && !current_user_can( 'delete_others_posts' ) )
+ 		if ( function_exists( 'current_user_can' ) && !current_user_can( 'manage_options' ) )
  			return false;
 
  		$wp_admin_bar->add_menu( array(
