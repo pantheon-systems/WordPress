1ae9d672fe3c05f78b71415b4ae7cb7dfe0368c9
Josh Koenig <josh@getpantheon.com>
2016-04-12 16:13:15 -0700

    Use our USER_AGENT_HTTPS header to set the default scheme

--- wp-config.php
+++ wp-config.php
@@ -67,8 +67,16 @@ else:
 
     /** A couple extra tweaks to help things run well on Pantheon. **/
     if (isset($_SERVER['HTTP_HOST'])) {
-      define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST']);
-      define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST']);
+        // HTTP is still the default scheme for now. 
+        $scheme = 'http';
+        // If we have detected that the end use is HTTPS, make sure we pass that
+        // through here, so <img> tags and the like don't generate mixed-mode
+        // content warnings.
+        if (isset($_SERVER['HTTP_USER_AGENT_HTTPS']) && $_SERVER['HTTP_USER_AGENT_HTTPS'] == 'ON') {
+            $scheme = 'https';
+        }
+        define('WP_HOME', $scheme . '://' . $_SERVER['HTTP_HOST']);
+        define('WP_SITEURL', $scheme . '://' . $_SERVER['HTTP_HOST']);
     }
     // Don't show deprecations; useful under PHP 5.5
     error_reporting(E_ALL ^ E_DEPRECATED);
@@ -79,7 +87,7 @@ else:
 
     // FS writes aren't permitted in test or live, so we should let WordPress know to disable relevant UI
     if ( in_array( $_ENV['PANTHEON_ENVIRONMENT'], array( 'test', 'live' ) ) && ! defined( 'DISALLOW_FILE_MODS' ) ) :
-      define( 'DISALLOW_FILE_MODS', true );
+        define( 'DISALLOW_FILE_MODS', true );
     endif;
 
   else:
