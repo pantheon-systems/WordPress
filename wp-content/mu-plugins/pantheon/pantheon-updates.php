44766413d107ed5e685d185ce78b9db3322d2e1d
ataylorme <andrew@ataylor.me>
2017-07-18 17:08:28 -0500

    Switch from h3 to paragraph tag

--- wp-content/mu-plugins/pantheon/pantheon-updates.php
+++ wp-content/mu-plugins/pantheon/pantheon-updates.php
@@ -46,9 +46,9 @@ function _pantheon_upstream_update_notice() {
 	$latest_wp_version = _pantheon_get_latest_wordpress_version();
     ?>
     <div class="update-nag">
-		<h3>
-			"WordPress <?php echo $latest_wp_version; ?> is available! Please update to WordPress <?php echo $latest_wp_version; ?> from <a href="https://dashboard.pantheon.io/sites/<?php echo $_ENV['PANTHEON_SITE']; ?>">your Pantheon dashboard</a>.
-		</h3>
+		<p style="font-size: 14px; font-weight: bold; margin: 0 0 0.5em 0;">
+			WordPress <?php echo $latest_wp_version; ?> is available! Please update from <a href="https://dashboard.pantheon.io/sites/<?php echo $_ENV['PANTHEON_SITE']; ?>">your Pantheon dashboard</a>.
+		</p>
 		For details on applying updates, see <a href="https://pantheon.io/docs/upstream-updates/" target="_blank">the Applying Upstream Updates documentation</a>. <br />
 		If you need help, open a support chat on Pantheon.
 	</div>
