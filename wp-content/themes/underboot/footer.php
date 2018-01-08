314c1f7536cf25734e76ed82796d88ad25852e9f
Sandy <sandy@qiigo.com>
2018-01-08 16:09:14 -0500

    adding theme folder underboot

---
+++ wp-content/themes/underboot/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package underboot
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+        Theme <i>Underboot</i> is Underscores dev theme + Bootstrap + NavWalker <span class="sep"> | </span> <a href="https://automattic.com/" rel="designer">Underscores.me</a>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
