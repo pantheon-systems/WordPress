0fbfbc71dc8b8cb425a61c92357a6061a78afdd4
Pantheon Upstream <developers+pantheon-upstream@getpantheon.com>
2018-04-04 06:33:15 -0700

    #137: Add Pantheon login form customization [shipit]
    Including pull request https://github.com/pantheon-systems/WordPress/pull/137 in branch update.

--- wp-content/mu-plugins/pantheon.php
+++ wp-content/mu-plugins/pantheon.php
@@ -1,16 +1,17 @@
 <?php
 /*
   Plugin Name: Pantheon
-  Plugin URI: http://www.getpantheon.com/
+  Plugin URI: https://pantheon.io/
   Description: Building on Pantheon's and WordPress's strengths, together.
   Version: 0.1
   Author: Pantheon
-  Author URI: http://getpantheon.com
+  Author URI: https://pantheon.io/
 */
 
 if ( isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ) :
 
 require_once( 'pantheon/pantheon-page-cache.php' );
 require_once( 'pantheon/pantheon-updates.php' );
+require_once( 'pantheon/pantheon-login-form-mods.php' );
 
 endif; # Ensuring that this is on Pantheon
