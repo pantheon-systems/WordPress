4b925062521664e007801fe78b36d94d91311011
Kyle Taylor <kyletaylored@gmail.com>
2021-03-11 15:23:50 -0600

    Enable plugins/themes

--- private/scripts/wp-site-import.php
+++ private/scripts/wp-site-import.php
@@ -1,5 +1,11 @@
 <?php
 
+// Enable Ocean theme
+echo "Enabling WP plugins and themes...\n";
+passthru("wp theme enable oceanwp --activate");
+passthru("wp plugin activate pantheon-advanced-page-cache wp-native-php-sessions ocean-extra");
+
+
 // Import data into WordPress
 echo "Importing default content...\n";
 passthru("wp import ../data/sample-data.xml --authors=skip");
