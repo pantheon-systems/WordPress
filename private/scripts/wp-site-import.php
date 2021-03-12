0d7a7cca62ecbd01c9a1987334fe823198c23165
Kyle Taylor <kyletaylored@gmail.com>
2021-03-12 11:17:54 -0600

    Fix WordPress importer

--- private/scripts/wp-site-import.php
+++ private/scripts/wp-site-import.php
@@ -5,7 +5,7 @@ $path  = $_SERVER['DOCUMENT_ROOT'] . '/private/data';
 // Enable Ocean theme
 echo "Enabling WP plugins and themes...\n";
 system("wp theme enable oceanwp --activate");
-system("wp plugin activate pantheon-advanced-page-cache wp-native-php-sessions ocean-extra");
+system("wp plugin install pantheon-advanced-page-cache wp-native-php-sessions ocean-extra wordpress-importer --activate");
 
 
 // Import data into WordPress
