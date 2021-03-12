75717ed889e4eecd8be4a75f1262770f47026e5e
Kyle Taylor <kyletaylored@gmail.com>
2021-03-12 10:00:26 -0600

    Update site import script

--- private/scripts/wp-site-import.php
+++ private/scripts/wp-site-import.php
@@ -1,12 +1,14 @@
 <?php
+// Get paths for imports
+$path  = $_SERVER['DOCUMENT_ROOT'] . '/private/data';
 
 // Enable Ocean theme
 echo "Enabling WP plugins and themes...\n";
-passthru("wp theme enable oceanwp --activate");
-passthru("wp plugin activate pantheon-advanced-page-cache wp-native-php-sessions ocean-extra");
+system("wp theme enable oceanwp --activate");
+system("wp plugin activate pantheon-advanced-page-cache wp-native-php-sessions ocean-extra");
 
 
 // Import data into WordPress
 echo "Importing default content...\n";
-passthru("wp import ../data/sample-data.xml --authors=skip");
+system("wp import ${path}/sample-data.xml --authors=skip");
 echo "Import complete.\n";
