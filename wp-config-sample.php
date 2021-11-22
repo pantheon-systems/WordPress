58e756cdc97b4f310a212e70ad31d2df6749cfb9
samanmn325 <49198507+samanmn325@users.noreply.github.com>
2021-11-22 11:31:45 +0330

    Update wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -56,6 +56,8 @@ define( 'AUTH_SALT',        'put your unique phrase here' );
 define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
 define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
 define( 'NONCE_SALT',       'put your unique phrase here' );
+define('JWT_AUTH_SECRET_KEY', 'your-top-secrect-key');
+
 
 /**#@-*/
 
