201b89c1ea156010ec00f124c8c099f1f53153ec
Brian Weaver <brian.weaver@pantheon.io>
2022-04-06 16:07:45 -0400

    Define FS_METHOD in pantheon mu-plugin: this setting can result in a significant performance improvement for sites that have highly populated directories. This setting has been documented previously at https://pantheon.io/docs/plugins-known-issues#define-fs_method

--- wp-content/mu-plugins/pantheon.php
+++ wp-content/mu-plugins/pantheon.php
@@ -3,7 +3,7 @@
  * Plugin Name: Pantheon
  * Plugin URI: https://pantheon.io/
  * Description: Building on Pantheon's and WordPress's strengths, together.
- * Version: 0.1
+ * Version: 0.2
  * Author: Pantheon
  * Author URI: https://pantheon.io/
  *
@@ -18,5 +18,17 @@ if ( isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ) {
 	}
 	if ( ! defined('RETURN_TO_PANTHEON_BUTTON') || RETURN_TO_PANTHEON_BUTTON ) {
 		require_once 'pantheon/pantheon-login-form-mods.php';
+	}
+	if ( ! defined( 'FS_METHOD' ) ) {
+		/**
+		 * When this constant is not set, WordPress writes and then deletes a 
+		 * temporary file to determine if it has direct access to the filesystem,
+		 * which we already know to be the case.  This multiplies filesystem 
+		 * operations and can degrade performance of the filesystem as a whole in
+		 * the case of large sites that do a lot of filesystem operations.  
+		 * Setting this constant to 'direct' tells WordPress to assume it has 
+		 * direct access and skip creating the extra temporary file.
+		 */
+		define( 'FS_METHOD', 'direct' );
 	}
 } // Ensuring that this is on Pantheon.
