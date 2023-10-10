263ede5320564080594180761581dce58357ddf0
cfp96 <carfelcoper@gmail.com>
2023-10-10 16:27:21 -0500

    CU-85zu3jcr3 - WordPress Sentry necessary configuration has been added

--- wp-config.php
+++ wp-config.php
@@ -75,7 +75,13 @@ if ( ! defined( 'WP_DEBUG' ) ) {
 
 /* That's all, stop editing! Happy Pressing. */
 
-
+if (isset($_ENV['PANTHEON_ENVIRONMENT'])) {
+	$secrets_json_text = file_get_contents('/files/private/secrets.json');
+	$data = json_decode($secrets_json_text, TRUE);
+	define('WP_SENTRY_PHP_DSN', $data['WP_SENTRY_PHP_DSN'] ? $data['WP_SENTRY_PHP_DSN'] : "");
+	define('WP_SENTRY_ENV', $_ENV['PANTHEON_ENVIRONMENT'] == "live" ? "production" : 'develop');
+	define('WP_SENTRY_ERROR_TYPES', E_ALL & ~E_DEPRECATED & ~E_NOTICE & ~E_USER_DEPRECATED & ~E_WARNING & ~E_USER_WARNING);
+}
 
 
 /** Absolute path to the WordPress directory. */
